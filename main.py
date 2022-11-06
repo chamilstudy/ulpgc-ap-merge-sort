5bbdd391ec184f20d60994047b8c9d4317f11dd7
Chamil José Cruz Razeq <chamil@Chamils-MacBook-Pro.home>
2022-11-06 16:46:39 +0000

    algorithm developed

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from solve import *
 
-first_line = input().split()
-numValues  = int(first_line[0])
-
-items = []
-for j in range(1, numValues+1):
-    parts      = input().split()
-    items.append (int(parts[0]))
+items = [1,9,4,9,9,4,0,5,9,0]
 
 solve(items)
 print(items)
+print((int)(5/2))
+
+def test():
+    first_line = input().split()
+    numValues  = int(first_line[0])
+
+    items = []
+    for j in range(1, numValues+1):
+        parts      = input().split()
+        items.append (int(parts[0]))
--- solve.py
+++ solve.py
@@ -3,16 +3,29 @@ def solve(items):
     """
     Sort the given list of items in ascending order
     """
-
+    solved = []
     def merge(left, mid, right):
-        # solve it here!
-        ...
-    
-    
+        #for k in range(left, right):
+        #    for i in range(left, right):
+        #        if items[i] > items[i+1]:
+        #            aux = items[i]
+        #            items[i] = items[i+1]
+        #            items[i+1] = aux
+        
+        for i in range(left, mid + 1):
+            for j in range(mid, right + 1):
+                if items[i] > items[j]:
+                    aux = items.pop(j)
+                    items.insert(i, aux)
+            
+
     def merge_sort(left, right):
         # solve it here!
-        ...
-    
-    
+        if left < right:
+            mid = (int) ((left + right) / 2)
+            merge_sort(left, mid)
+            merge_sort(mid + 1, right)
+            merge(left, mid + 1, right)
+        
     merge_sort (0, len(items)-1)
     return
